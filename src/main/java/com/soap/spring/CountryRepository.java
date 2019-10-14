dc212328aecfeee39fee762eea4fa82ade1cc40f
joaosilva <jv_rss@hotmail.com>
2019-10-14 11:12:14 -0300

    Adicionando dados

--- src/main/java/com/soap/spring/CountryRepository.java
+++ src/main/java/com/soap/spring/CountryRepository.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 import com.baeldung.springsoap.gen.Country;
+import com.baeldung.springsoap.gen.Currency;
 
 @Component
 public class CountryRepository {
@@ -16,7 +17,29 @@ public class CountryRepository {
  
     @PostConstruct
     public void initData() {
-        // initialize countries map
+    	Country spain = new Country();
+        spain.setName("Spain");
+        spain.setCapital("Madrid");
+        spain.setCurrency(Currency.EUR);
+        spain.setPopulation(46704314);
+
+        countries.put(spain.getName(), spain);
+
+        Country poland = new Country();
+        poland.setName("Poland");
+        poland.setCapital("Warsaw");
+        poland.setCurrency(Currency.PLN);
+        poland.setPopulation(38186860);
+
+        countries.put(poland.getName(), poland);
+
+        Country uk = new Country();
+        uk.setName("United Kingdom");
+        uk.setCapital("London");
+        uk.setCurrency(Currency.GBP);
+        uk.setPopulation(63705000);
+
+        countries.put(uk.getName(), uk);
     }
  
     public Country findCountry(String name) {
